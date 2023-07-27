bd6883c26a9944e277cdf811ee9bd2bf550fe02b
Starman_x64 <starman.x64@gmail.com>
2023-07-27 10:02:23 +0800

    update

--- README.md
+++ README.md
@@ -2,11 +2,12 @@
 Detailing the events of the Kessoku Band faction in r/place 2023.
 
 
-## Vision
+## brain dump
 - scrollable, interactive timeline (probably vertical)
 - might be cool to have the canvas in the background which changes depending on far you scroll down.
-- plus an additional one in just document form
-- add page to generate event json files to push into the main branch
+- plus an additional page with the events document form
+- add page to generate event json files to push into the main branch (maybe?)
+- could use discord as an interface for adding events
 
 ## Naming Conventions
 Event and chapter files are named by the following convention:
@@ -14,4 +15,4 @@ Event and chapter files are named by the following convention:
 For example:
 ```2023-07-26_05-39_test-chapter.json```
 ```2023-07-26_05-42_test-event-2.json```
-Times are in UTC.
+Times are in UTC-7.
--- js/documentation.js
+++ js/documentation.js
@@ -1,3 +1,5 @@
+const DAY_ONE = "2023-07-20";
+
 // load events json
 fetch("https://starman-x64.github.io/kessoku-place-2023/data/events.json")
   .then((response) => response.json())
@@ -5,20 +7,37 @@ fetch("https://starman-x64.github.io/kessoku-place-2023/data/events.json")
 
 // loop through each chapter and add it and its events to the screen
 function renderEvents(eventsObject) {
+  
   let documentation = document.getElementById("documentation");
   eventsObject.forEach(chapter => {
-    // create div to contain contents
+    // create div and table to contain events
     let chapterDiv = document.createElement("div");
+    let eventsTable = document.createElement("table");
     chapterDiv.innerHTML = `<h2>${chapter.name}</h2>`
+    chapterDiv.classList.add("chapter");
+    eventsTable.classList.add("events-table");
 
     // make sure events are sorted by time
     let sortedEvents = chapter.events.sort((a, b) => (a.timeStamp > b.timeStamp ? 1 : -1));
     // loop through each event and write it to the chapterDiv
     sortedEvents.forEach(event => {
-      // let eventDiv = document.createElement("div");
-      console.log(event.name);
+      let eventTR = document.createElement("tr");
+      let timeStamp = document.createElement("td");
+      let description = document.createElement("td");
+
+      timeStamp.innerHTML = event.timeStamp;
+      timeStamp.classList.add("time-stamp");
+      description.innerHTML = event.description;
+      timeStamp.classList.add("event-description");
+      eventTR.classList.add("event");
+
+      eventTR.appendChild(timeStamp);
+      eventTR.appendChild(description);
+      
+      eventsTable.appendChild(eventTR);
     });
 
+    chapterDiv.appendChild(eventsTable);
     documentation.appendChild(chapterDiv);
     console.log(chapter);
   });
